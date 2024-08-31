382f83a9f3f277fd0ad4e01f0b37f48db8e18e97
Matheus-OD074 <70017961+Matheus-OD074@users.noreply.github.com>
2024-08-31 20:32:30 -0300

    Estrutura do projeto

---
+++ app/src/main/java/br/edu/isfp/dmo5/exavaliativo2dmos5/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.isfp.dmo5.exavaliativo2dmos5.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.isfp.dmo5.exavaliativo2dmos5.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
